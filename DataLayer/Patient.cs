8d13ff91652cbedca77fdf54f8f1b89c02ea5d7d
Jakub Czabator <jczabator@gmail.com>
2024-01-02 01:01:11 +0100

    Add initial repo

---
+++ DataLayer/Patient.cs
@@ -0,0 +1,16 @@
+﻿namespace PatientsMedicines.DataLayer
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string SocialNumber { get; set; }
+        public int Age { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public decimal BMI { get; set; }
+    }
+}
