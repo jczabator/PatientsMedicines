d61b30bb4eca2fc42bb3a966019fb38ac428ce9a
Jakub Czabator <jczabator@gmail.com>
2024-01-02 15:31:26 +0100

    Add DbContext

---
+++ DataLayer/MedicineResearchDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PatientsMedicines.DataLayer
+{
+    public class MedicineResearchDbContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<MedicineGroup> MedicineGroups { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientMedicineGroup> PatientMedicineGroups { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = MedicineResearch; Integrated Security = True");
+        }                
+    }
+}
