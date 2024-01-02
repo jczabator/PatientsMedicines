0dc2594f3d56b2775f63c6c0f6123cbe51941289
Jakub Czabator <jczabator@gmail.com>
2024-01-02 01:10:16 +0100

    Create Medicine.cs

---
+++ DataLayer/Medicine.cs
@@ -0,0 +1,11 @@
+﻿namespace PatientsMedicines.DataLayer
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string MedicineSynonym { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public string ExpiryDate { get; set; }
+    }
+}
